1043b1045fa9ce1b280154f89b1f42062d0c6ad7
sambajarju2-png <sambajarju2@gmail.com>
2026-03-21 18:18:05 +0100

    Fix: sanity.config.ts import path

--- apps/landing/sanity.config.ts
+++ apps/landing/sanity.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { schemaTypes } from "./sanity/schemas";
+import { schemaTypes } from "./src/sanity/schemas";
 
 export default defineConfig({
   name: "paywatch",
